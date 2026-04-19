53ab1b588c89e862a22783b96f2faa97e39908f9
DanielPincu <ghepin01@easv365.dk>
2026-04-19 13:01:23 +0200

    updated

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const http = require('http')
 const WebSocket = require('ws')
 
 const server = http.createServer((req, res) => {
-  if (req.url.startsWith('/temp')) {
-    const value = new URL(req.url, 'http://x').searchParams.get('value')
-
-    wss.clients.forEach(client => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify({ type: 'temp', value: Number(value) }))
+  if (req.method === 'GET' && req.url.startsWith('/temp')) {
+    wss.clients.forEach(c => {
+      if (c.readyState === WebSocket.OPEN) {
+        c.send(JSON.stringify({ type: 'temp', value: +req.url.split('=').pop() }))
       }
     })
   }
