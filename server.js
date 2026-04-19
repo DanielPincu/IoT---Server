38835fb77037fe4cbeb43017f27707fe6f1e5819
DanielPincu <ghepin01@easv365.dk>
2026-04-19 12:05:15 +0200

    test

--- server.js
+++ server.js
@@ -1,61 +1,24 @@
 const http = require('http')
 const WebSocket = require('ws')
 
-const port = process.env.PORT || 10000
-
 const server = http.createServer((req, res) => {
   if (req.url.startsWith('/temp')) {
-    const url = new URL(req.url, 'http://localhost')
-    const value = url.searchParams.get('value')
+    const value = new URL(req.url, 'http://x').searchParams.get('value')
 
-    console.log('TEMP FROM DEVICE:', value)
-
-    wss.clients.forEach(client => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify({
-          type: 'temp',
-          value: Number(value)
-        }))
+    wss.clients.forEach(c => {
+      if (c.readyState === WebSocket.OPEN) {
+        c.send(JSON.stringify({ type: 'temp', value: Number(value) }))
       }
     })
-
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('OK')
-    return
   }
 
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
   res.end('OK')
 })
 
 const wss = new WebSocket.Server({ noServer: true })
 
 server.on('upgrade', (req, socket, head) => {
-  wss.handleUpgrade(req, socket, head, (ws) => {
-    wss.emit('connection', ws, req)
-  })
+  wss.handleUpgrade(req, socket, head, ws => wss.emit('connection', ws))
 })
 
-console.log('WS running on port', port)
-
-server.listen(port)
-
-wss.on('connection', (ws) => {
-  console.log('client connected')
-
-  ws.on('message', (msg) => {
-    const text = msg.toString()
-    console.log('received:', text)
-
-    // broadcast to all
-    wss.clients.forEach(client => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(text)
-      }
-    })
-  })
-
-  ws.on('close', () => {
-    console.log('client disconnected')
-  })
-})
+server.listen(10000)
