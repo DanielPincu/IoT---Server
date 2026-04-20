81d9768ce734d3a1d2d91f7ef58dc212cce50f34
DanielPincu <ghepin01@easv365.dk>
2026-04-20 12:27:37 +0200

    Experimental

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
+const http = require('http')
 const WebSocket = require('ws')
 
-const wss = new WebSocket.Server({ port: 10000 })
-
-wss.on('connection', (ws) => {
-  ws.on('message', (msg) => {
-    const text = msg.toString()
-
-    wss.clients.forEach(c => {
-      if (c.readyState === WebSocket.OPEN) {
-        c.send(text)
-      }
-    })
-  })
+const server = http.createServer((req, res) => {
+  if (req.url === '/') return res.end('OK')
+  res.end()
 })
+
+const wss = new WebSocket.Server({ server })
+
+wss.on('connection', ws =>
+  ws.on('message', msg =>
+    wss.clients.forEach(c =>
+      c.readyState === WebSocket.OPEN && c.send(msg.toString())
+    )
+  )
+)
+
+server.listen(10000)
